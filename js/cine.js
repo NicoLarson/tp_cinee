d24e9ee4fd65f61cebf81264ed236f048308ed5e
Nicolas_Yang <nicolas.yang.1@gmail.com>
2020-05-13 14:50:39 +0200

    Delete console.log

--- js/cine.js
+++ js/cine.js
@@ -1,37 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-const target = document.querySelector("section")
+    const target = document.querySelector("section")
 
-//* Creation tableaux    
+    //* Creation tableaux    
     let cine = [{
-            image: "./asset/img/king-kong.jpg",
-            titre: "King Kong",
-            date: 1933,
-            genre:"Horreur, fantastique",
-            dvd:"22 novembre 2005",
-            real:"Merian C.Cooper, Ernest B"
-        },{
-            image: "./asset/img/camonte.jpg",
-            titre: "Scarface",
-            date: 1932,
-            genre:"Policier, action",
-            dvd:"23 décembre 2005",
-            real:"Howard Hawks"
-        },{
-            image: "./asset/img/autant-le-vent-little.png",
-            titre: "Autant en emporte le vent",
-            date: 1939,
-            genre:"Dramatique, guerre",
-            dvd:"19 mars 2005",
-            real:"Victor Fleming"
-        },
-    ]
+        image: "./asset/img/king-kong.jpg",
+        titre: "King Kong",
+        date: 1933,
+        genre: "Horreur, fantastique",
+        dvd: "22 novembre 2005",
+        real: "Merian C.Cooper, Ernest B"
+    }, {
+        image: "./asset/img/camonte.jpg",
+        titre: "Scarface",
+        date: 1932,
+        genre: "Policier, action",
+        dvd: "23 décembre 2005",
+        real: "Howard Hawks"
+    }, {
+        image: "./asset/img/autant-le-vent-little.png",
+        titre: "Autant en emporte le vent",
+        date: 1939,
+        genre: "Dramatique, guerre",
+        dvd: "19 mars 2005",
+        real: "Victor Fleming"
+    }, ]
 
-console.log(cine.length)
-
-    for (let i=0; i < cine.length ; i++){
-    target.innerHTML +=
-    `<figure>
+    for (let i = 0; i < cine.length; i++) {
+        target.innerHTML +=
+            `<figure>
     <img src="${cine[i].image}" alt="${cine[i].titre}">
     <figcaption>
         <h2>${cine[i].titre}</h2>
